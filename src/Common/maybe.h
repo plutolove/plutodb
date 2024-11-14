3632151ab80560ef96f123ec8ecbd21e0345be91
Shangqi Meng <meng-shangqi@qq.com>
2024-11-14 12:13:53 +0800

    Update maybe.h

--- src/Common/maybe.h
+++ src/Common/maybe.h
@@ -1,224 +1,225 @@
 #pragma once
-#include <cstdint>
-#include <string>
+#include <memory>
+#include <shared_mutex>
 #include <type_traits>
 #include <variant>
-#include <vector>
 
+#include "Common/exception.h"
+#include "Common/log.h"
 #include "Common/type_trait.h"
 #include "fmt/format.h"
 
-namespace pluto {
-
-struct ErrorFrame {
-  ErrorFrame(const std::string& file, uint64_t line, const std::string& func,
+class ErrorFrame final {
+ public:
+  ErrorFrame(const std::string& f, const std::string& func, size_t l,
              const std::string& err)
-      : file_(file), line_(line), function_(func), err_msg(err) {}
+      : file(f), function_name(func), line(l), err_msg(err) {}
 
   std::string debugString() const {
-    return fmt::format("{}:{} {}\n\t{}\n", file_, line_, function_, err_msg);
+    return fmt::format("{}:{} {}:\n\t{}\n", file, line, function_name, err_msg);
   }
 
-  std::string file_;
-  uint64_t line_;
-  std::string function_;
+ private:
+  std::string file;
+  std::string function_name;
+  size_t line;
   std::string err_msg;
 };
 
-class Error {
+class Error final {
  public:
   Error() {}
 
-  Error(const std::string& file, uint64_t line, const std::string& func,
-        const std::string& err) {
-    error_frames_.emplace_back(file, line, func, err);
+  Error(const Error&) = default;
+  Error(Error&&) = default;
+
+  Error&& addErrorFrame(const std::string& file, const std::string& func,
+                        size_t line, const std::string& err) {
+    error_frames.emplace_back(file, func, line, err);
+    return std::move(*this);
   }
 
-  void addFrame(const std::string& file, uint64_t line, const std::string& func,
-                const std::string& err) {
-    error_frames_.emplace_back(file, line, func, err);
+  Error&& addErrorFrame(ErrorFrame&& err) {
+    error_frames.push_back(std::move(err));
+    return std::move(*this);
   }
 
   std::string debugString() const {
     std::string res;
-    for (auto& frame : error_frames_) {
+    for (auto& frame : error_frames) {
       res.append(frame.debugString());
     }
     return res;
   }
 
+  static Error CheckFailedError() { return Error(); }
+
  private:
-  std::vector<ErrorFrame> error_frames_;
+  std::vector<ErrorFrame> error_frames;
 };
 
-using ErrorPtr = std::shared_ptr<Error>;
+using ErrorPtr = std::unique_ptr<Error>;
 
-template <typename T>
-inline ErrorPtr JustGetError() {
-  return std::make_shared<Error>();
-}
-
-inline ErrorPtr&& JustErrorAddStackFrame(ErrorPtr&& err,
-                                         const std::string& file, int64_t line,
-                                         const std::string& func,
-                                         const std::string& message) {
-  err->addFrame(file, line, func, message);
-  return std::move(err);
-}
-
-template <typename T, typename = guard::Guard>
+template <typename T, typename = type_trait::guard::Guard>
 class Maybe;
 
 template <typename T>
-class Maybe<T,
-            guard::ValueGuard<not(IsScalar_v<T> || std::is_same_v<T, void>) &&
-                              not std::is_reference_v<T>>> {
+class Maybe<T, type_trait::guard::ValueGuard<not(type_trait::IsScalar_v<T> ||
+                                                 std::is_same_v<T, void>) and
+                                             not std::is_reference_v<T>>>
+    final {
+  using ValueT = std::unique_ptr<T>;
+
  public:
-  using ValueType = std::shared_ptr<T>;
-  Maybe(const T& data) : data_or_error_(std::make_shared<T>(data)) {}
-  Maybe(T&& data) : data_or_error_(std::make_shared<T>(std::move(data))) {}
+  Maybe(const T& data) : data_or_err_(std::make_unique<T>(data)) {}
+  Maybe(T&& data) : data_or_err_(std::make_unique<T>(std::move(data))) {}
 
-  Maybe(const std::shared_ptr<T>& data) : data_or_error_(data) {}
-  Maybe(std::shared_ptr<T>&& data) : data_or_error_(std::move(data)) {}
+  Maybe(std::unique_ptr<T> ptr) : data_or_err_(std::move(ptr)) {}
+  Maybe(const std::unique_ptr<T>& ptr) : data_or_err_(std::move(ptr)) {}
 
-  Maybe(const Error& error) : data_or_error_(std::make_shared<Error>(error)) {}
-  Maybe(const std::shared_ptr<Error>& error) : data_or_error_(error) {}
+  Maybe(const Error& err) : data_or_err_(std::make_unique<Error>(err)) {}
+  Maybe(Error&& err) : data_or_err_(std::make_unique<Error>(std::move(err))) {}
+
+  Maybe(ErrorPtr err) : data_or_err_(std::move(err)) {}
+
+  Maybe(Maybe&& data) : data_or_err_(std::move(data.data_or_err_)) {}
 
   Maybe(const Maybe&) = default;
-  Maybe(Maybe&& other) : data_or_error_(std::move(other.data_or_error_)) {}
   ~Maybe() = default;
 
-  bool isOk() const {
-    return std::holds_alternative<ValueType>(data_or_error_);
-  }
+  bool isOk() const { return std::holds_alternative<ValueT>(data_or_err_); }
 
-  ValueType Data_UserCanNotUse() const { return std::get<0>(data_or_error_); }
+  ValueT data() { return std::move(std::get<ValueT>(data_or_err_)); }
 
-  ErrorPtr stackError() const { return std::get<1>(data_or_error_); }
-
-  std::string errorStr() const {
-    return std::get<1>(data_or_error_)->debugString();
-  }
+  ErrorPtr error() { return std::get<1>(data_or_err_); }
 
  private:
-  std::variant<ValueType, ErrorPtr> data_or_error_;
+  std::variant<ValueT, ErrorPtr> data_or_err_;
 };
 
 template <typename T>
-class Maybe<T, guard::ValueGuard<IsScalar_v<T> && not std::is_same_v<T, void> &&
-                                 not std::is_reference_v<T>>> {
+class Maybe<T, type_trait::guard::ValueGuard<std::is_same_v<T, void>>> final {
  public:
-  Maybe(const T& data) : data_or_error_(data) {}
-  Maybe(T&& data) : data_or_error_(std::move(data)) {}
+  Maybe(const Error& err) : data_or_err_(std::make_unique<Error>(err)) {}
+  Maybe(Error&& err) : data_or_err_(std::make_unique<Error>(std::move(err))) {}
 
-  Maybe(const Error& error) : data_or_error_(std::make_shared<Error>(error)) {}
-  Maybe(const std::shared_ptr<Error>& error) : data_or_error_(error) {}
+  Maybe(ErrorPtr err) : data_or_err_(std::move(err)) {}
+
+  Maybe(Maybe&& data) : data_or_err_(std::move(data.data_or_err_)) {}
 
   Maybe(const Maybe&) = default;
-  Maybe(Maybe&& other) = default;
   ~Maybe() = default;
 
-  bool isOk() const { return std::holds_alternative<T>(data_or_error_); }
-
-  T Data_UserCanNotUse() const { return std::get<0>(data_or_error_); }
-
-  ErrorPtr stackError() const { return std::get<1>(data_or_error_); }
-
-  std::string errorStr() const {
-    return std::get<1>(data_or_error_)->debugString();
-  }
-
- private:
-  std::variant<T, ErrorPtr> data_or_error_;
-};
-
-template <typename T>
-class Maybe<T, guard::ValueGuard<std::is_same_v<T, void>>> {
- public:
   static Maybe Ok() { return Maybe(); }
-  Maybe(const Error& error) : data_or_error_(std::make_shared<Error>(error)) {}
-  Maybe(const std::shared_ptr<Error>& error) : data_or_error_(error) {}
 
-  Maybe(const Maybe&) = default;
-  Maybe(Maybe&& other) = default;
-  ~Maybe() = default;
+  void data() const {}
 
-  bool isOk() const { return std::holds_alternative<void*>(data_or_error_); }
+  bool isOk() const { return std::holds_alternative<void*>(data_or_err_); }
 
-  void* Data_UserCanNotUse() const { return nullptr; }
-
-  ErrorPtr stackError() const { return std::get<1>(data_or_error_); }
-
-  std::string errorStr() const {
-    return std::get<1>(data_or_error_)->debugString();
-  }
+  ErrorPtr error() { return std::move(std::get<ErrorPtr>(data_or_err_)); }
 
  private:
-  Maybe() : data_or_error_(nullptr) {}
-  std::variant<void*, ErrorPtr> data_or_error_;
+  Maybe() : data_or_err_(nullptr) {}
+  std::variant<void*, ErrorPtr> data_or_err_;
 };
 
 template <typename T>
-class Maybe<T,
-            guard::ValueGuard<not(IsScalar_v<T> || std::is_same_v<T, void>) &&
-                              std::is_reference_v<T>>> {
+class Maybe<T, type_trait::guard::ValueGuard<type_trait::IsScalar_v<T> and
+                                             not std::is_reference_v<T>>>
+    final {
  public:
-  using ValueType = std::add_pointer_t<T>;
-  Maybe(T data) : data_or_error_(&data) {}
+  Maybe(T data) : data_or_err_(data) {}
 
-  Maybe(const Error& error) : data_or_error_(std::make_shared<Error>(error)) {}
-  Maybe(const std::shared_ptr<Error>& error) : data_or_error_(error) {}
+  Maybe(const Error& err) : data_or_err_(std::make_unique<Error>(err)) {}
+  Maybe(Error&& err) : data_or_err_(std::make_unique<Error>(std::move(err))) {}
+
+  Maybe(ErrorPtr err) : data_or_err_(std::move(err)) {}
+
+  Maybe(Maybe&& data) = default;
 
   Maybe(const Maybe&) = default;
-  Maybe(Maybe&& other) = default;
   ~Maybe() = default;
 
-  bool isOk() const {
-    return std::holds_alternative<ValueType>(data_or_error_);
-  }
+  bool isOk() const { return std::holds_alternative<T>(data_or_err_); }
 
-  T Data_UserCanNotUse() const { return *std::get<0>(data_or_error_); }
+  T data() const { return std::get<0>(data_or_err_); }
 
-  ErrorPtr stackError() const { return std::get<1>(data_or_error_); }
-
-  std::string errorStr() const {
-    return std::get<1>(data_or_error_)->debugString();
-  }
+  ErrorPtr error() { return std::move(std::get<ErrorPtr>(data_or_err_)); }
 
  private:
-  std::variant<ValueType, ErrorPtr> data_or_error_;
+  std::variant<T, ErrorPtr> data_or_err_;
 };
 
 template <typename T>
-bool JustIsOk(const Maybe<T>& val) {
+class Maybe<T, type_trait::guard::ValueGuard<std::is_reference_v<T>>> final {
+  using ValueT = typename std::remove_reference_t<T>;
+  using PtrT = ValueT*;
+
+ public:
+  Maybe(T data) : data_or_err_(&data) {}
+
+  Maybe(const Error& err) : data_or_err_(std::make_unique<Error>(err)) {}
+  Maybe(Error&& err) : data_or_err_(std::make_unique<Error>(std::move(err))) {}
+
+  Maybe(ErrorPtr err) : data_or_err_(std::move(err)) {}
+
+  Maybe(Maybe&& data) = default;
+  Maybe(const Maybe&) = default;
+  ~Maybe() = default;
+
+  bool isOk() const { return std::holds_alternative<T>(data_or_err_); }
+
+  const T& data() const { return *std::get<0>(data_or_err_); }
+  T& data() { return *std::get<0>(data_or_err_); }
+
+  ErrorPtr error() { return std::move(std::get<ErrorPtr>(data_or_err_)); }
+
+ private:
+  std::variant<PtrT, ErrorPtr> data_or_err_;
+};
+
+inline ErrorPtr&& AddErrFrame(ErrorPtr&& error, const std::string& file,
+                              const std::string& func, size_t line,
+                              const std::string& err) {
+  error->addErrorFrame(file, func, line, fmt::format("Error Expr: {}", err));
+  return std::move(error);
+}
+
+template <typename T>
+inline bool JustIsOk(const Maybe<T>& val) {
   return val.isOk();
 }
 
-inline Error&& AddErrorFrame(Error&& err, const std::string& file,
-                             uint64_t line, const std::string& func,
-                             const std::string& msg) {
-  err.addFrame(file, line, func, msg);
-  return std::move(err);
-}
+#define STRINGIZEIMP(x) #x
+#define STRINGIZE(x) STRINGIZEIMP(x)
 
-#define NewError(msg) \
-  std::make_shared<pluto::Error>(__FILE__, __LINE__, __FUNCTION__, msg);
-
-#define OK_OR_RETURN(expr, msg) \
-  do {                          \
-    if (expr) {                 \
-      return NewError(msg);     \
-    }                           \
+#define OK_OR_RETURN(expr)                                   \
+  do {                                                       \
+    if (not(expr)) {                                         \
+      return Error::CheckFailedError().addErrorFrame(        \
+          __FILE__, __FUNCTION__, __LINE__,                  \
+          fmt::format("Check Failed: {}", STRINGIZE(expr))); \
+    }                                                        \
   } while (0)
 
-#define JUST(...)                                                           \
-  ({                                                                        \
-    auto&& _just_value_ = (__VA_ARGS__);                                    \
-    if (!JustIsOk(_just_value_)) {                                          \
-      return AddErrorFrame(std::move(*_just_value_.stackError()), __FILE__, \
-                           __LINE__, __FUNCTION__, #__VA_ARGS__);           \
-    }                                                                       \
-    std::forward<decltype(_just_value_)>(_just_value_);                     \
-  }).Data_UserCanNotUse()
+#define JUST_VAL_WRAPPER(...) __VA_ARGS__
 
-}  // namespace pluto
+#define JUST(...)                                                              \
+  ({                                                                           \
+    auto&& just_value = JUST_VAL_WRAPPER(__VA_ARGS__);                         \
+    if (not JustIsOk(just_value)) {                                            \
+      return AddErrFrame(just_value.error(), __FILE__, __FUNCTION__, __LINE__, \
+                         STRINGIZE(__VA_ARGS__));                              \
+    }                                                                          \
+    std::forward<decltype(just_value)>(just_value);                            \
+  }).data()
+
+#define OK_OR_THROW(...)                                                  \
+  do {                                                                    \
+    auto&& just_value = JUST_VAL_WRAPPER(__VA_ARGS__);                    \
+    if (not JustIsOk(just_value)) {                                       \
+      auto err_ = AddErrFrame(just_value.error(), __FILE__, __FUNCTION__, \
+                              __LINE__, STRINGIZE(__VA_ARGS__));          \
+      ThrowException("{}", err_->debugString());                          \
+    }                                                                     \
+  } while (0)
